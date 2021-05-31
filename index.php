72a89baf77058d54d4c37bad0d41df466e150c9b
TFBosoN <tfboson@pm.me>
2021-05-31 23:48:06 +0300

    feature 2

--- index.php
+++ index.php
@@ -272,19 +272,16 @@ switch($names){
 	var imgwidth = <?=$imgh/9*16;?>;
 	var imgheight = <?=$imgh;?>;
 	const bars = document.querySelectorAll('.bar');
-const progress = document.querySelectorAll('.progress');
+	const progress = document.querySelectorAll('.progress');
 
-bars.forEach((bar, index) => {
-  const randomWidth = Math.floor((Math.random() * 65) + 10);
-  bar.style.width = `${randomWidth}%`;
-
-  progress[index].addEventListener('mouseover', () => {
-    const randomTiming = Math.floor((Math.random() * 2) + 2);
-    console.log(randomTiming);
-    bar.style.transitionDuration = `${randomTiming}s`;
-    bar.style.width = '100%';
-  });
-})
+	bars.forEach((bar, index) => {
+		const randomTiming = Math.floor((Math.random() * 2) + 2);
+		bar.style.transitionDuration = `${randomTiming}s`;
+		bar.style.width = '100%';
+		if(bar.style.width == '100%'){
+			bar.style.width = '0%';
+		}
+	})
 	</script>
 	<script src='//enviosity.com/assets/main.js'></script>
 </body>
