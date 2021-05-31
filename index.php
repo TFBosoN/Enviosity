13dab5e4ebdb6a12b76c9e54e3e002c12b903f1e
TFBosoN <tfboson@pm.me>
2021-05-31 23:49:12 +0300

    feature 3

--- index.php
+++ index.php
@@ -275,13 +275,19 @@ switch($names){
 	const progress = document.querySelectorAll('.progress');
 
 	bars.forEach((bar, index) => {
+	  const randomWidth = Math.floor((Math.random() * 65) + 10);
+	  bar.style.width = `${randomWidth}%`;
+
+	  progress[index].addEventListener('mouseover', () => {
 		const randomTiming = Math.floor((Math.random() * 2) + 2);
+		console.log(randomTiming);
 		bar.style.transitionDuration = `${randomTiming}s`;
 		bar.style.width = '100%';
 		if(bar.style.width == '100%'){
 			bar.style.width = '0%';
 		}
-	})
+	  });
+	});
 	</script>
 	<script src='//enviosity.com/assets/main.js'></script>
 </body>
