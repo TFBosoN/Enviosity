42f95728f1812cfba94663f32b043f3930df1c91
TFBosoN <tfboson@pm.me>
2021-06-11 16:56:30 +0300

    rates

--- index.php
+++ index.php
@@ -35,7 +35,6 @@ $names = $names[$count];
 
 $promote_name = array();
 $promote_name[] = "Cabbage Head";
-$promote_name[] = "Mr. Encryptocurrency";
 
 $categories = array_filter(explode("#", file_get_contents("./envi_names.txt")));
 $rcat = rand(1, count($categories)-1); //Choosing rand category
