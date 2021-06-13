316eb24377b764f7d61a5acdc14101eb56ab4692
TFBosoN <tfboson@pm.me>
2021-06-13 16:51:19 +0300

    changed promo name

--- index.php
+++ index.php
@@ -40,11 +40,13 @@ $names = array_filter(explode("\n", $categories[$rcat]));
 $count = rand(2,count($names)-1);
 
 
-if(isset($_COOKIE['no_hackermans']) || true){
+if(isset($_COOKIE['no_promo']) /*|| true*/){
 	$promote_name[] = $names[$count];
 }else{
-	$promote_name[] = "Hackiosity";
-	setcookie( "no_hackermans", true, time()+10 );
+	$promote_name[] = $names[$count];
+	$promote_name[] = $names[$count];
+	$promote_name[] = "Eulaviosity";
+	setcookie( "no_promo", true, time()+10 );
 }
 
 
