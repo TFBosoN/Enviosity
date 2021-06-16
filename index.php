b9b16f4a77e403e20ebd2427488d1191cbf0522b
TFBosoN <tfboson@pm.me>
2021-06-16 23:05:06 +0300

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $bg_images = json_decode(file_get_contents("./envi.json"));
 $imgh = 360;
 $zoom = 2;
 
-$without = "3 days without cursing";
+$without = "4 days without cursing";
 $phrase_fs = false;
 
 //COPIUM 
