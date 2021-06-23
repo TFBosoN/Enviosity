5a71332ec2560982ee2794cf0c73ee29b2dd991f
TFBosoN <tfboson@pm.me>
2021-06-23 05:05:56 +0300

    fix 10

--- chat/index.php
+++ chat/index.php
@@ -484,6 +484,7 @@ function badges(chan, user, isBot) {
 function testMessage(channel, user, message, self) { // Throw away when done
 	handleChat(channel || tmi.opts.channels[0], user || { 'display-name': 'Alca', emotes: null }, message || '(chompy) bttvNice domeHey domeLit splinCreep', self || false);
 }
+testMessage('TFBosoN', null, 'Hey~! This is a secret underground chat! You can use BOOBA and PagMan !');
 function chatNotice(information, noticeFadeDelay, level, additionalClasses) {
 	var ele = document.createElement('div');
 	
