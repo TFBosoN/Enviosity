7c3826d72fca374cf5eae5a3b8451e1e87fb4436
TFBosoN <tfboson@pm.me>
2021-06-23 23:31:47 +0300

    fix 4

--- chat/index.php
+++ chat/index.php
@@ -699,7 +699,7 @@ function hosting(channel, target, viewers, unhost) {
 			{"id": "5fdab6e1f0c5583492692c36", "code":"PEPSICLE", "imgType":"gif"},
 			{"id": "60a0bbcf67644f1d67e870d6", "code":"agreeGe", "imgType":"png"},
 			{"id": "60819d1f39b5010444d0427c", "code":"PepePls", "imgType":"png"},
-			{"id": "60586872dfaae06e7d60c464", "code":"HandsUp", "imgType":"png"},
+			{"id": "60ae8a98f8b3f62601c337c2", "code":"Kissahomie", "imgType":"gif"},
 			{"id": "60586872dfaae06e7d60c464", "code":"HandsUp", "imgType":"png"},
 			{"id": "60586872dfaae06e7d60c464", "code":"HandsUp", "imgType":"png"},
 		], "sharedEmotes":[]};
