8c0172f86bd324aa47c2af79aaf4d33914e5fe0a
TFBosoN <tfboson@pm.me>
2021-06-26 18:09:52 +0300

    update

--- index.php
+++ index.php
@@ -196,7 +196,9 @@ switch($names){
 	<meta name="keywords" content="enviosity, f2p, gamer, streamer, youtube, social, genshin, impact, genshin impact, slipper">
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Staatliches&display=swap" rel="stylesheet"> 
 	<!-- Fontawesome icons -->
 	<link rel="stylesheet" href="./assets/fa/css/all.min.css">
 	<link rel="stylesheet" href="./assets/main.css">
