2afe555d5279cd98f481cddc0c3a45dbf4eaf756
TFBosoN <tfboson@pm.me>
2021-06-30 21:06:15 +0300

    fix 1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if(isset($_GET['rolled'])){
 }
 
 
-if(!isset($_COOKIE['rolled']) && !isset($_GET['no_wish'])){
+if(!isset($_GET['rolled']) && !isset($_GET['no_wish'])){
 	header('Location: ./wish/', true, 302);
 	exit();
 }
