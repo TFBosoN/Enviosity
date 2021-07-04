6f19b6fd81057e1677fdddfceef981cb88f6f2d5
TFBosoN <tfboson@pm.me>
2021-07-04 13:29:15 +0300

    upd

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ switch($names){
 
 <html>
 <head>
-	<title>Enviosity LIVE NOW! 🔴</title>
+	<title>Enviosity</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 	<meta charset="utf-8">
 	<meta name="description" CONTENT="Enviosity's f2p website">
@@ -324,7 +324,7 @@ switch($names){
 				<a>GFUEL use code "ENVIOSITY" for 10% off!</a><br>
 				<?=($alarm)?"<br><br><a class='red'>".$alarm_msg."</a>":"";?>
 				<br>
-				<a href="./watch/" style="color:white; font-size: 26px;">Watch Enviosity LIVE NOW! 🔴</a><br>
+				<a href="./watch/" style="color:white; font-size: 26px;">Watch Enviosity</a><br>
 				<div class="lines">
 					<span><a class="youtube" href="https://youtube.com/Enviosity" target="_blank"><?=($alarm && !empty($alarm_icon))? $alarm_icon: '<i class="fab fa-youtube"></i>';?><br><l>Youtube</l></a></span>
 					<span><a class="twitch" href="https://www.twitch.tv/enviosity" target="_blank"><?=($alarm && !empty($alarm_icon))? $alarm_icon: '<i class="fab fa-twitch"></i>';?><br><l>Twitch</l></a></span>
